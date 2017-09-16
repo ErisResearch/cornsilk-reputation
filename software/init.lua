95b965aa6133b7d8b24b03a288e5b34d60e64f18
reveilled <reveilled1@gmail.com>
2017-09-16 17:09:11 -0400

    Updates for new NodeMCU API and use config.lua for storage management

--- software/init.lua
+++ software/init.lua
@@ -1,35 +1,39 @@
-function finish_startup()
+function finish_startup(val_table)
     dofile("roomba_serial.lua")
     dofile("mqtt_command.lua")
-	dofile("telnet.lua")
+    dofile("telnet.lua")
+    wifi.eventmon.unregister(wifi.eventmon.STA_GOT_IP)
     end
 
 function wifi_connect()
 
-	ssid = ""
-	wifi_password = ""
-	hostname = "catbot"
 
-	--print ("Connecting to "..ssid.." as "..hostname)
-    wifi.setmode(wifi.STATION)                                                          
-    wifi.sta.eventMonReg(wifi.STA_GOTIP, function() wifi.sta.eventMonStop() finish_startup() end)
-    wifi.sta.eventMonStart(100)
+    --print ("Connecting to "..ssid.." as "..hostname)
+    wifi.setmode(wifi.STATION)    
+    wifi_config = {}
+    wifi_config.ssid = getconfigvalue("ssid")
+    wifi_config.pwd = getconfigvalue("wifikey")
+    hostname = getconfigvalue("hostname")
+
+    wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, finish_startup)
     wifi.sta.sethostname(hostname)
-    wifi.sta.config(ssid,wifi_password)
+    wifi.sta.config(wifi_config)
     end    
 
 function daily_restart()
-	hours_ticked = hours_ticked + 1
-	if hours_ticked >= 24 then
-		node.restart()
-	end
+    hours_ticked = hours_ticked + 1
+    if hours_ticked >= 24 then
+        node.restart()
+    end
 end
 
 function startup()
     node.setcpufreq(node.CPU160MHZ)
-	--restart every 24 hours
-	tmr.register(6,3600000,tmr.ALARM_AUTO, daily_restart )
-	tmr.start(6)
+    --restart every 24 hours
+    tmr.register(6,3600000,tmr.ALARM_AUTO, daily_restart )
+    tmr.start(6)
+    dofile("config.lua")
+    loadconfig("config.json")
     wifi_connect()
     end
 
