08d4884c00e554a8efa6306f9492e88f343f4aa5
Reveilled <reveilled1@gmail.com>
2020-02-08 11:46:29 -0800

    Fixed up MQTT, debug print functionality, and added an empty config

--- software/init.lua
+++ software/init.lua
@@ -1,11 +1,20 @@
+function debug_print(outstr)
+	--only print if GPIO4 is set to low
+	--do nothing if set to high
+	--note: Eagle schematic has GPIO 4/5 swapped
+	if gpio.read(2) == gpio.LOW then
+		print("DEBUG: "..outstr)
+	end
+end
+
 function finish_startup(val_table)
-	print("In finish_startup: IP = "..val_table.IP)
---	sntp.sync()
---    dofile("mqtt_command.lua")
-	print("starting up telnet")
+	debug_print("In finish_startup: IP = "..val_table.IP)
+	--sntp.sync()
+    dofile("mqtt_command.lua")
+	debug_print("starting up telnet")
     dofile("telnet.lua")
     wifi.eventmon.unregister(wifi.eventmon.STA_GOT_IP)
-	print("done")
+	debug_print("done")
     end
 
 function wifi_connect()
@@ -14,7 +23,7 @@ function wifi_connect()
 	wifi_password = getconfigvalue("wifikey")
 	hostname = getconfigvalue("hostname")
 
-    print ("Connecting to "..ssid.." as "..hostname)
+    debug_print ("Connecting to "..ssid.." as "..hostname)
     wifi.setmode(wifi.STATION)   
 	wifi.setphymode(wifi.PHYMODE_N)
     wifi_config = {}
@@ -28,7 +37,7 @@ function wifi_connect()
     wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, finish_startup)
     wifi.sta.sethostname(hostname)
 	wifi.sta.config(wifi_config)
-	print("Waiting on an IP")
+	debug_print("Waiting on an IP")
     end    
 
 --function daily_restart()
@@ -42,6 +51,8 @@ function wifi_connect()
 
 function startup()
     node.setcpufreq(node.CPU160MHZ)
+	--setup GPIO 4 for debug info
+	gpio.mode(2, gpio.INPUT)
 	--restart every 24 hours
 --	cron.schedule("0 13 * * *", daily_restart)
 --	tmr.register(6,3600000,tmr.ALARM_AUTO, daily_restart )
@@ -52,6 +63,9 @@ function startup()
     wifi_connect()
     end
 
+-- Wait 5 seconds before actually doing anythin
+-- That way you don't end up in a panic loop that
+-- is faster than you can upload a replacement file for
 hours_ticked = 0
 startupTimer = tmr.create()
 startupTimer:register(5000, tmr.ALARM_SINGLE,startup)
--- software/mqtt_command.lua
+++ software/mqtt_command.lua
@@ -1,51 +1,48 @@
 m= nil
 host = getconfigvalue("mqtt_server")
 user = getconfigvalue("mqtt_user")
+client_name = getconfigvalue("mqttt_name")
 password = getconfigvalue("mqtt_password")
-command_topic = "desk/command"
-status_topic = "desk/status"
+command_topic = getconfigvalue("mqtt_command_topic")
+status_topic = getconfigvalue("mqtt_status_topic")
 
 previous_message = nil
 
 function receive_message(client, topic, data)
 	if topic == command_topic then
-		roomba_set_status(data)
+		debug_print("MQTT Received command "..data)
 	end
 end
 
-function post_connect(client)
-    end
-
 function mqtt_send(text)
     previous_message = text
-    --m:publish(status_topic,text,0,0, function(client) roomba_debug_print ("sent "..text) end)
-    m:publish(status_topic,text,0,0, function(client) end)
+    m:publish(status_topic,text,0,0, function(client) debug_print ("sent "..text) end)
     end
 
 function on_connect(client)
-    --print("connected")
-    --client:subscribe(command_topic,0,function(client) print("subscribe success") update_status(nil) end)
-    client:subscribe(command_topic,0,function(client) update_status(nil) end)
+	debug_print("MQTT Connected")
+    client:subscribe(command_topic,0,function(client) debug_print("subscribe success") end)
+	mqtt_send("Hello!")
     end
 
 function on_offline(client)
-    --print("offline")
+    debug_print("MQTT offline")
     end
 
 function on_failure(client, reason)
-    --print("Failed reason: "..reason)
+    debug_print("MQTT Failed reason: "..reason)
     end
 
 
-function setup_mqtt_command(host, user, password, command_topic)
-    --print("Connecting to "..host.." as "..user)
-    m = mqtt.Client("catbot",120,user,password)
+function setup_mqtt_command(host, user, password, client_name, command_topic)
+    --debug_print("Connecting to "..host.." as "..user)
+    m = mqtt.Client(client_name,120,user,password)
     m:lwt("/lwt","offline",0,0)
     m:on("connect",on_connect)
     m:on("offline",on_offline)
     m:on("message",receive_message)
-    m:connect(host,1883,0, on_connect, on_failure)
+    m:connect(host,1883,false, on_connect, on_failure)
     end
 
 
-setup_mqtt_command(host, user, password, command_topic)
+setup_mqtt_command(host, user, password, client_name, command_topic)
